264058de17a1824c4cb8a2385768cc94165b4046
Bill Tihen <btihen@gmail.com>
2024-05-20 16:38:25 +0200

    update model overrides since params assign  not nil when blank

--- app/graphs/nodes/person.rb
+++ app/graphs/nodes/person.rb
@@ -2,19 +2,20 @@ module Nodes
   class Person
     include ApacheAge::Vertex
 
-    attribute :first_name, :string
-    attribute :last_name, :string
-    attribute :given_name, :string
-    attribute :nick_name, :string
-    attribute :gender, :string
+    attribute :first_name, :string, default: nil
+    attribute :last_name, :string, default: nil
+    attribute :given_name, :string, default: nil
+    attribute :nick_name, :string, default: nil
+    attribute :gender, :string, default: nil
 
     validates :gender, :first_name, :last_name, :given_name, :nick_name,
               presence: true
 
     def initialize(**attributes)
       super
-      self.nick_name ||= first_name
-      self.given_name ||= last_name
+      # use unless present? since attributes when empty sets to "" by default
+      self.nick_name = first_name unless nick_name.present?
+      self.given_name = last_name unless given_name.present?
     end
   end
 end
